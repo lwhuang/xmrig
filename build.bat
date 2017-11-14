f8a556ad2511f7845efb5abe31638bdadf4c5513
lwhuang <lw.member@gmail.com>
2017-11-14 23:08:05 +0800

    add build.bat

---
+++ build.bat
@@ -0,0 +1,14 @@
+SET rootpath=%~dp0
+
+cd src\3rdparty\libuv
+call vcbuild release x64
+
+
+cd %rootpath%
+set bdir=buildx64
+rmdir /s /q %bdir%
+mkdir %bdir%
+cd %bdir%
+
+
+cmake -DUV_LIBRARY=%rootpath%src\3rdparty\libuv\Release\lib\libuv.lib -DUV_INCLUDE_DIR=%rootpath%src\3rdparty\libuv\include -DMHD_INCLUDE_DIR=%rootpath%lib\libmicrohttpd-0.9.55-w32-bin\x86_64\VS2015\Release-static -DMHD_LIBRARY=%rootpath%lib\libmicrohttpd-0.9.55-w32-bin\x86_64\VS2015\Release-static\libmicrohttpd.lib -G "Visual Studio 14 2015 Win64" ..
